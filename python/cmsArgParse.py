64b137add7f950190bc58d6ca92aa9be885be656
Kevin Pedro <kpedro88@gmail.com>
2023-07-07 16:51:58 -0500

    attempt to simplify

--- python/cmsArgParse.py
+++ python/cmsArgParse.py
@@ -4,20 +4,20 @@ import sys as _sys
 def cmsArgParse(base=argparse.ArgumentParser, **kwargs):
     # makes a derived class for the given base class; this allows starting from a user's derived ArgumentParser class with custom behavior
     class cmsArgumentParser(base):
-        # get only args that come after separator
-        def _fix_args(self, args, sep='--'):
-                try:
-                    sep_index = args.index(sep)
-                except ValueError:
-                    raise ValueError("Arguments must come after {} separator".format(sep))
-                args = args[sep_index+1:]
+        # get only args that come after the name of the configuration script (following VarParsing convention)
+        def _fix_args(self, args):
+                print(args)
+                config_index = next((index for index, arg in enumerate(args) if arg.endswith('.py')), -1)
+                if config_index==-1:
+                    raise RuntimeError("No configuration file found ending in .py")
+                args = args[config_index+1:]
                 return args
         # arg default handling taken from argparse
         def _handle_args(self, args=None):
             if args is None:
                 # args default to the system args
                 args = _sys.argv[1:]
-                # check for separator by default for command-line input only
+                # check for config by default for command-line input only
                 args = self._fix_args(args)
             else:
                 # make sure that args are mutable
@@ -41,7 +41,7 @@ if __name__=="__main__":
             None
         def testDefault(self):
             orig_argv = deepcopy(_sys.argv)
-            _sys.argv = ['cmsRun','--strict','config.py','--','-t']
+            _sys.argv = ['cmsRun','--strict','config.py','-t']
             parser = cmsArgParse()
             parser.add_argument("-t","--test",default=False,action="store_true",help="test arg")
             args = parser.parse_args()
@@ -49,9 +49,9 @@ if __name__=="__main__":
             self.assertEqual(args,expected)
         def testNoSep(self):
             orig_argv = deepcopy(_sys.argv)
-            _sys.argv = ['cmsRun','--strict','config.py','-t']
+            _sys.argv = ['cmsRun','--strict','config','-t']
             parser = cmsArgParse()
             parser.add_argument("-t","--test",default=False,action="store_true",help="test arg")
-            self.assertRaises(ValueError,parser.parse_args)
+            self.assertRaises(RuntimeError,parser.parse_args)
 
     unittest.main()
