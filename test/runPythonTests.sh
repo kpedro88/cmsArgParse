c43b777b8486b6c11f6760fd780a6f6283349a19
Kevin Pedro <kpedro88@gmail.com>
2023-07-07 15:49:22 -0500

    fix path

--- test/runPythonTests.sh
+++ test/runPythonTests.sh
@@ -3,12 +3,12 @@
 # Pass in name and status
 function die { echo $1: status $2 ;  exit $2; }
 
-for file in ${CMSSW_BASE}/src/FWCore/ParameterSet/python/*.py
+for file in ${CMSSW_BASE}/src/FWCore/Args/python/*.py
 do
   bn=`basename $file`
   if [ "$bn" != "__init__.py" ]; then
      bnm=${bn%.*} 
-     python3 -m FWCore.ParameterSet."$bnm" || die "unit tests for $bn failed" $?
+     python3 -m FWCore.Args."$bnm" || die "unit tests for $bn failed" $?
   fi
 done
 
